fb5d6411b436e8e181dac6d29ba9e8a9bec4b10c
PanasyukVladimir <v.panasyuk@i-teko.ru>
2024-02-11 19:56:31 +0300

    add modal authorization

--- reactApp/src/components/login-control/component.jsx
+++ reactApp/src/components/login-control/component.jsx
@@ -1,17 +1,29 @@
 import { useContext } from "react";
 import { UserContext } from "../contexts/user";
-import {Button} from "../button/component";
+import {  useState } from "react";
+import { Modal } from "../modal/component";
+import { Button } from "../button/component";
+import { AuthorizationForm } from "../authorization-form/component.jsx";
 
 export const LoginControl = () => {
     const {user, setUser} = useContext(UserContext);
-    return <div>
-        {user ? <>
-                    <span>{user}</span>
-                    <Button text="Logout"
-                            onClick={() => setUser(null)}/>
-                </>  
-              : <Button onClick={() => setUser("Ivan")} 
-                        text="Login"/> 
-        } 
-    </div>
-}
+    const [isOpen, setIsOpen] = useState(false);
+    const closeModal = () => setIsOpen(false);    
+
+    return  <div>
+                
+                { isOpen && (<Modal onClose={closeModal}>
+                                <AuthorizationForm onLogin={closeModal}/>
+                            </Modal>)
+                }
+                { user ? (<>
+                                <span>{user}</span>
+                                <Button text="Logout"
+                                        onClick={() => setUser(null)}/>
+                            </>)
+                        
+                        : (<Button onClick={() => setIsOpen(true)}
+                                    text="Login"/>)
+                }
+            </div>
+};
